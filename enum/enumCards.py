1de04b5f09961c91988753cbb23d0c0d53d12ab2
unknown <wangqianjiang@live.com>
2020-12-10 19:45:09 -0600

    format by black

--- enum/enumCards.py
+++ enum/enumCards.py
@@ -1,30 +1,34 @@
 from enum import Enum
 from pprint import pprint
+
 """
 Class level function
 Class level attribute
 """
 
-class Suits(Enum):
-    SPADE=1
-    CLUB=2
-    DIAMOND=3
-    HEART=4
 
-class Faces(Enum): # the value must to be unique
-    ACE='A'
-    TWO=2 
-    THREE=3
-    FOUR=4
-    FIVE=5
-    SIX=6
-    SEVEN=7
-    EIGHT=8
-    NINE=9
-    TEN=10
-    JACK='J'
-    QUEEN='Q'
-    KING='K'
+class Suits(Enum):
+    SPADE = 1
+    CLUB = 2
+    DIAMOND = 3
+    HEART = 4
+
+
+class Faces(Enum):  # the value must to be unique
+    ACE = "A"
+    TWO = 2
+    THREE = 3
+    FOUR = 4
+    FIVE = 5
+    SIX = 6
+    SEVEN = 7
+    EIGHT = 8
+    NINE = 9
+    TEN = 10
+    JACK = "J"
+    QUEEN = "Q"
+    KING = "K"
+
 
 class Card:
     def __init__(self, face, suit):
@@ -33,7 +37,7 @@ class Card:
 
     def __repr__(self):
         return f"({self.face.value}, {self.suit.name})"
-    
+
     def getValue(self):
         if self.face in (Faces.JACK, Faces.QUEEN, Faces.KING):
             return 10
@@ -41,18 +45,20 @@ class Card:
             return 1
         return self.face.value
 
+
 class Deck:
     def __init__(self):
         self.deck = [Card(f, s) for s in Suits for f in Faces]
-                
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     for face in Faces:
         print(face)
 
     for suit in Suits:
         print(suit)
-    
-    print("48:",Faces['TWO'])
+
+    print("48:", Faces["TWO"])
     member = Faces.TEN
     print(member.name)
     print(member.value)
